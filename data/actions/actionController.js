a20d4de04bb6a94ea61ee82963b3627b3bbd7f8f
basilcea <ogbonna.basil3@gmmail.com>
2019-07-12 10:26:16 +0100

    create action controllers

---
+++ data/actions/actionController.js
@@ -0,0 +1,51 @@
+const Actions = require('./actionModel');
+const status = (res, data, status) => {
+    return res.status(status).json(data);
+  };
+
+  const GetAction = async( req , res) => {
+    const {actionId} = req.params
+    try{
+        const data = Actions.get(actionId)
+        status(res ,data, 200)
+    }
+    catch(err){
+        status(res ,'Cannot Get Actions', 500)
+    }
+  }
+
+  const AddAction = async(req ,res) => {
+    const {description, notes} = req.body
+    const {id} = req.params
+    try{
+        const data = Projects.insert({description,notes, project_id:id})
+        status(res ,data , 200)
+    }
+    catch(err){
+        status(res, 'Cannot Add Actions', 500)
+    }
+}
+
+const UpdateProject = async(req, res) => {
+    const {actionId} = req.params;
+    const {description , notes} = req.body
+
+    try{
+      const data = Projects.update(actionId ,{description , notes})
+      status(res ,data , 200)
+  }
+  catch(err){
+      status(res, 'Cannot Update Actions', 500)
+  }
+}
+
+const DeleteProject = async(req, res) => {
+    const {actionId} = req.params;
+    try{
+        await Projects.delete(actionId)
+        status(res, "Action Deleted", 200)
+    }
+    catch(err){
+        status(res , 'Cannot Delete Action' , 500)
+    }
+}
